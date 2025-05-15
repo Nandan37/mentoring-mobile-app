aa11983cecf69c5f2fd6a55aaf832e41c8c14ee9
vishwanath1004 <50612405+vishwanath1004@users.noreply.github.com>
2025-05-15 10:28:16 +0530

    Restricted pages update

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -8,9 +8,9 @@ window["env"] = {
 	  termsOfServiceUrl:'https://shikshalokam.org/mentoring/term-of-use',
 	  supportEmail: "mentoredtest1@yopmail.com",
     recaptchaSiteKey:"6LfWEKYpAAAAACxKbR7H42o3BwbJkJA06vIM_6Ea", // Google reCAPTCHA site key for enabling CAPTCHA verification
-    restictedPages: ['PROFILE_PAGE','LOGIN_ACTIVITY_PAGE','CHANGE_PASSWORD_PAGE','LOGIN_PAGE','REGISTER_PAGE','RESET_PASSWORD_PAGE','OTP_PAGE','EDIT_PROFILE'], // Array of page IDs to restrict access. Add page IDs from `src/app/core/constants/page.ids.ts`
-    isAuthBypassed: true, // Set to "true" to disable the default user authentication service, allowing bypass of login
-    unauthorizedRedirectUrl: "/tabs/home", // URL to redirect users to if their session expires (e.g., "/auth/login")
+    restictedPages: [], // Array of page IDs to restrict access. Add page IDs from `src/app/core/constants/page.ids.ts`
+    isAuthBypassed: false, // Set to "true" to disable the default user authentication service, allowing bypass of login
+    unauthorizedRedirectUrl: "/auth/login", // URL to redirect users to if their session expires (e.g., "/auth/login")
     password:{ 
       minLength:10, // Minimum password length requirement (e.g., 10 characters)
       regexPattern: "^(?=(?:.*[A-Z]){2})(?=(?:.*[0-9]){2})(?=(?:.*[!@#%$&()\\-`.+,]){3}).{11,}$", // Regex pattern for strong password validation
