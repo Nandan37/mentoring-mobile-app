7f7b91e7acaf8bda15c790a0e979d2cf83d6ac7d
rohxnn <rohanreji123@gmail.com>
2025-10-15 17:49:46 +0530

    fix(3885): fix language issue

--- src/app/pages/language/language.page.ts
+++ src/app/pages/language/language.page.ts
@@ -4,14 +4,12 @@ import { localKeys } from 'src/app/core/constants/localStorage.keys';
 import { LocalStorageService, ToastService } from 'src/app/core/services';
 import { languagesList } from 'src/app/core/constants/languageConstant';
 import { ProfileService } from 'src/app/core/services/profile/profile.service';
-
 @Component({
   selector: 'app-language',
   templateUrl: './language.page.html',
   styleUrls: ['./language.page.scss'],
 })
 export class LanguagePage implements OnInit {
-
   public headerConfig: any = {
     // backButton: {
     //   label: 'LANGUAGE',
@@ -24,38 +22,32 @@ export class LanguagePage implements OnInit {
   };
   languagesList=languagesList;
   selectedLanguage: any;
-
   constructor(private localStorage: LocalStorageService,
               private translate: TranslateService,
               private toast: ToastService,
               private profile: ProfileService) { }
-
   ngOnInit() {
     this.localStorage.getLocalData(localKeys.SELECTED_LANGUAGE).then(data =>{
-      this.selectedLanguage = data.value;
+      this.selectedLanguage = data;
     })
   }
-
   onCardClick(event){
-    this.selectedLanguage=event.value;
+    this.selectedLanguage=event;
   }
-
   onSubmit(){
     let showProfileUpdateToast = false;
-    this.profile.updateLanguage({preferred_language:this.selectedLanguage}, showProfileUpdateToast).then((result)=>{
+    this.profile.updateLanguage({preferred_language:this.selectedLanguage.value}, showProfileUpdateToast).then((result)=>{
       if(result){
         this.setLanguage(this.selectedLanguage);
       }
     })
   }
-
   setLanguage(lang){
     this.localStorage.setLocalData(localKeys.SELECTED_LANGUAGE,lang).then(() =>{
-      this.translate.use(lang);
+      this.translate.use(lang.value);
       this.toast.showToast("LANGUAGE_CHANGED_SUCCESSFULLY","success");
     }).catch(error => {
       this.toast.showToast("ERROR_LANGUAGE_CHANGE","danger");
     })
   }
-
 }
