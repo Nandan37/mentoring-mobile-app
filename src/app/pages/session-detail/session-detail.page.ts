58cfb5f7139c692a8b79b18de46b41f6424cd803
rohxnn <rohanreji123@gmail.com>
2025-11-04 12:32:37 +0530

    use key insted of title

--- src/app/pages/session-detail/session-detail.page.ts
+++ src/app/pages/session-detail/session-detail.page.ts
@@ -187,7 +187,7 @@ export class SessionDetailPage implements OnInit, OnDestroy {
       this.startDate = (response.start_date>0)?new Date(response.start_date * 1000):this.startDate;
       this.endDate = (response.end_date>0)?new Date(response.end_date * 1000):this.endDate;
       this.platformOff = (response?.meeting_info?.platform == 'OFF') ? true : false;
-      if((!this.detailData.controls.some(obj =>  obj.title.toLowerCase() === 'mentor'))){
+      if((!this.detailData.controls.some(obj =>  obj.key === 'mentor_name'))){
         this.detailData.controls.push(
           {
             title: 'Mentor',
@@ -195,7 +195,7 @@ export class SessionDetailPage implements OnInit, OnDestroy {
           },
         );
       } 
-      if((this.isCreator || this.isConductor) && !this.detailData.controls.some(obj => obj.title.toLowerCase() === 'mentee count')){
+      if((this.isCreator || this.isConductor) && !this.detailData.controls.some(obj => obj.key === 'mentee_count')){
         
         this.detailData.controls.push(
           {
